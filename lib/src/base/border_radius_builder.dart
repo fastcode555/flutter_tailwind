7a3d6d781f1d05ef0ce43f52d2381e991935a5d4
Barry <87644022+fastcode555@users.noreply.github.com>
2024-08-19 21:42:39 +0700

    add the feature

--- lib/src/base/border_radius_builder.dart
+++ lib/src/base/border_radius_builder.dart
@@ -31,83 +31,43 @@ extension BorderRadiusBuilderExt<T extends BorderRadiusBuilder> on T {
 
   T get r10 => this..radius = 10.r;
 
-  T get r11 => this..radius = 11.r;
-
   T get r12 => this..radius = 12.r;
-
-  T get r13 => this..radius = 13.r;
 
   T get r14 => this..radius = 14.r;
 
-  T get r15 => this..radius = 15.r;
-
   T get r16 => this..radius = 16.r;
-
-  T get r17 => this..radius = 17.r;
 
   T get r18 => this..radius = 18.r;
 
-  T get r19 => this..radius = 19.r;
-
   T get r20 => this..radius = 20.r;
-
-  T get r21 => this..radius = 21.r;
 
   T get r22 => this..radius = 22.r;
 
-  T get r23 => this..radius = 23.r;
-
   T get r24 => this..radius = 24.r;
-
-  T get r25 => this..radius = 25.r;
 
   T get r26 => this..radius = 26.r;
 
-  T get r27 => this..radius = 27.r;
-
   T get r28 => this..radius = 28.r;
-
-  T get r29 => this..radius = 29.r;
 
   T get r30 => this..radius = 30.r;
 
-  T get r31 => this..radius = 31.r;
-
   T get r32 => this..radius = 32.r;
-
-  T get r33 => this..radius = 33.r;
 
   T get r34 => this..radius = 34.r;
 
-  T get r35 => this..radius = 35.r;
-
   T get r36 => this..radius = 36.r;
-
-  T get r37 => this..radius = 37.r;
 
   T get r38 => this..radius = 38.r;
 
-  T get r39 => this..radius = 39.r;
-
   T get r40 => this..radius = 40.r;
-
-  T get r41 => this..radius = 41.r;
 
   T get r42 => this..radius = 42.r;
 
-  T get r43 => this..radius = 43.r;
-
   T get r44 => this..radius = 44.r;
-
-  T get r45 => this..radius = 45.r;
 
   T get r46 => this..radius = 46.r;
 
-  T get r47 => this..radius = 47.r;
-
   T get r48 => this..radius = 48.r;
-
-  T get r49 => this..radius = 49.r;
 
   T get r50 => this..radius = 50.r;
 }
--- lib/src/base/font_size_builder.dart
+++ lib/src/base/font_size_builder.dart
@@ -47,83 +47,43 @@ extension FontSizeBuilderExt<T extends FontSizeBuilder> on T {
 
   T get f20 => this..fontSize = 20.sp;
 
-  T get f21 => this..fontSize = 21.sp;
-
   T get f22 => this..fontSize = 22.sp;
-
-  T get f23 => this..fontSize = 23.sp;
 
   T get f24 => this..fontSize = 24.sp;
 
-  T get f25 => this..fontSize = 25.sp;
-
   T get f26 => this..fontSize = 26.sp;
-
-  T get f27 => this..fontSize = 27.sp;
 
   T get f28 => this..fontSize = 28.sp;
 
-  T get f29 => this..fontSize = 29.sp;
-
   T get f30 => this..fontSize = 30.sp;
-
-  T get f31 => this..fontSize = 31.sp;
 
   T get f32 => this..fontSize = 32.sp;
 
-  T get f33 => this..fontSize = 33.sp;
-
   T get f34 => this..fontSize = 34.sp;
-
-  T get f35 => this..fontSize = 35.sp;
 
   T get f36 => this..fontSize = 36.sp;
 
-  T get f37 => this..fontSize = 37.sp;
-
   T get f38 => this..fontSize = 38.sp;
-
-  T get f39 => this..fontSize = 39.sp;
 
   T get f40 => this..fontSize = 40.sp;
 
-  T get f41 => this..fontSize = 41.sp;
-
   T get f42 => this..fontSize = 42.sp;
-
-  T get f43 => this..fontSize = 43.sp;
 
   T get f44 => this..fontSize = 44.sp;
 
-  T get f45 => this..fontSize = 45.sp;
-
   T get f46 => this..fontSize = 46.sp;
-
-  T get f47 => this..fontSize = 47.sp;
 
   T get f48 => this..fontSize = 48.sp;
 
-  T get f49 => this..fontSize = 49.sp;
-
   T get f50 => this..fontSize = 50.sp;
-
-  T get f51 => this..fontSize = 51.sp;
 
   T get f52 => this..fontSize = 52.sp;
 
-  T get f53 => this..fontSize = 53.sp;
-
   T get f54 => this..fontSize = 54.sp;
-
-  T get f55 => this..fontSize = 55.sp;
 
   T get f56 => this..fontSize = 56.sp;
 
-  T get f57 => this..fontSize = 57.sp;
-
   T get f58 => this..fontSize = 58.sp;
-
-  T get f59 => this..fontSize = 59.sp;
 
   T get f60 => this..fontSize = 60.sp;
 }
