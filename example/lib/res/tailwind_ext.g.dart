edd91f1a6e5fb330f42969f182cbfbad02310a1c
Barry <87644022+fastcode555@users.noreply.github.com>
2024-08-27 22:51:59 +0700

    feature: part of 和 part的妙用，需要使用此功能，对library进行重构

---
+++ example/lib/res/tailwind_ext.g.dart
@@ -0,0 +1,191 @@
+part of 'tailwind_ext.dart';
+
+extension ColorExt<T extends ColorBuilder> on T {
+  T get secondaryColor => this..color(Colours.secondaryColor);
+
+  T get appMain => this..color(Colours.appMain);
+
+  T get gray33 => this..color(Colours.gray33);
+
+  T get gray99 => this..color(Colours.gray99);
+
+  T get mainTextColor => this..color(Colours.mainTextColor);
+
+  T get textHintColor => this..color(Colours.textHintColor);
+
+  T get lineColor => this..color(Colours.lineColor);
+
+  T get colorE8e8e8 => this..color(Colours.colorE8e8e8);
+
+  T get color11151C => this..color(Colours.color11151C);
+
+  T get colorFfbd49 => this..color(Colours.colorFfbd49);
+
+  T get colorFE2042 => this..color(Colours.colorFE2042);
+
+  T get backgroundColor => this..color(Colours.backgroundColor);
+
+  T get disableColor => this..color(Colours.disableColor);
+
+  T get mainText2 => this..color(Colours.mainText2);
+
+  T get x00212639 => this..color(Colours.x00212639);
+
+  T get primary => this..color(Colours.primary);
+
+  T get x26ffffff => this..color(Colours.x26ffffff);
+
+  T get redColor => this..color(Colours.redColor);
+
+  T get transparent => this..color(Colours.transparent);
+
+  T get ccfa94fb => this..color(Colours.ccfa94fb);
+
+  T get ccad83e4 => this..color(Colours.ccad83e4);
+
+  T get ccfdf194 => this..color(Colours.ccfdf194);
+
+  T get ff4d4d4d => this..color(Colours.ff4d4d4d);
+
+  T get ff2e3a56 => this..color(Colours.ff2e3a56);
+
+  T get btnDisableColor => this..color(Colours.btnDisableColor);
+
+  T get unselectedColor => this..color(Colours.unselectedColor);
+
+  T get blueAccent => this..color(Colours.blueAccent);
+
+  T get purpleAccent => this..color(Colours.purpleAccent);
+
+  T get unSelectedColor => this..color(Colours.unSelectedColor);
+
+  T get gray => this..color(Colours.gray);
+
+  T get ff0c4e5f => this..color(Colours.ff0c4e5f);
+
+  T get ff172f35 => this..color(Colours.ff172f35);
+
+  T get ff333333 => this..color(Colours.ff333333);
+
+  T get ff363636 => this..color(Colours.ff363636);
+
+  T get ffb7d547 => this..color(Colours.ffb7d547);
+
+  T get fff46b29 => this..color(Colours.fff46b29);
+
+  T get redFF5F5F => this..color(Colours.redFF5F5F);
+
+  T get appbarActiveColor => this..color(Colours.appbarActiveColor);
+
+  T get orange => this..color(Colours.orange);
+
+  T get colorGrey => this..color(Colours.colorGrey);
+
+  T get colorGrey495057 => this..color(Colours.colorGrey495057);
+
+  T get colorBlack494949 => this..color(Colours.colorBlack494949);
+
+  T get colorRed3A28 => this..color(Colours.colorRed3A28);
+
+  T get colorWhiteF8F9FA => this..color(Colours.colorWhiteF8F9FA);
+
+  T get colorWhiteF5F5 => this..color(Colours.colorWhiteF5F5);
+
+  T get colorGreyF0F0 => this..color(Colours.colorGreyF0F0);
+}
+
+extension BorderColorExt<T extends BorderColorBuilder> on T {
+  T get borderSecondaryColor => this..border(Colours.secondaryColor);
+
+  T get borderAppMain => this..border(Colours.appMain);
+
+  T get borderGray33 => this..border(Colours.gray33);
+
+  T get borderGray99 => this..border(Colours.gray99);
+
+  T get borderMainTextColor => this..border(Colours.mainTextColor);
+
+  T get borderTextHintColor => this..border(Colours.textHintColor);
+
+  T get borderLineColor => this..border(Colours.lineColor);
+
+  T get borderColorE8e8e8 => this..border(Colours.colorE8e8e8);
+
+  T get borderColor11151C => this..border(Colours.color11151C);
+
+  T get borderColorFfbd49 => this..border(Colours.colorFfbd49);
+
+  T get borderColorFE2042 => this..border(Colours.colorFE2042);
+
+  T get borderBackgroundColor => this..border(Colours.backgroundColor);
+
+  T get borderDisableColor => this..border(Colours.disableColor);
+
+  T get borderMainText2 => this..border(Colours.mainText2);
+
+  T get borderX00212639 => this..border(Colours.x00212639);
+
+  T get borderPrimary => this..border(Colours.primary);
+
+  T get borderX26ffffff => this..border(Colours.x26ffffff);
+
+  T get borderRedColor => this..border(Colours.redColor);
+
+  T get borderTransparent => this..border(Colours.transparent);
+
+  T get borderCcfa94fb => this..border(Colours.ccfa94fb);
+
+  T get borderCcad83e4 => this..border(Colours.ccad83e4);
+
+  T get borderCcfdf194 => this..border(Colours.ccfdf194);
+
+  T get borderFf4d4d4d => this..border(Colours.ff4d4d4d);
+
+  T get borderFf2e3a56 => this..border(Colours.ff2e3a56);
+
+  T get borderBtnDisableColor => this..border(Colours.btnDisableColor);
+
+  T get borderUnselectedColor => this..border(Colours.unselectedColor);
+
+  T get borderRedAccent => this..border(Colours.redAccent);
+
+  T get borderBlueAccent => this..border(Colours.blueAccent);
+
+  T get borderPurpleAccent => this..border(Colours.purpleAccent);
+
+  T get borderUnSelectedColor => this..border(Colours.unSelectedColor);
+
+  T get borderGray => this..border(Colours.gray);
+
+  T get borderFf0c4e5f => this..border(Colours.ff0c4e5f);
+
+  T get borderFf172f35 => this..border(Colours.ff172f35);
+
+  T get borderFf333333 => this..border(Colours.ff333333);
+
+  T get borderFf363636 => this..border(Colours.ff363636);
+
+  T get borderFfb7d547 => this..border(Colours.ffb7d547);
+
+  T get borderFff46b29 => this..border(Colours.fff46b29);
+
+  T get borderRedFF5F5F => this..border(Colours.redFF5F5F);
+
+  T get borderAppbarActiveColor => this..border(Colours.appbarActiveColor);
+
+  T get borderOrange => this..border(Colours.orange);
+
+  T get borderColorGrey => this..border(Colours.colorGrey);
+
+  T get borderColorGrey495057 => this..border(Colours.colorGrey495057);
+
+  T get borderColorBlack494949 => this..border(Colours.colorBlack494949);
+
+  T get borderColorRed3A28 => this..border(Colours.colorRed3A28);
+
+  T get borderColorWhiteF8F9FA => this..border(Colours.colorWhiteF8F9FA);
+
+  T get borderColorWhiteF5F5 => this..border(Colours.colorWhiteF5F5);
+
+  T get borderColorGreyF0F0 => this..border(Colours.colorGreyF0F0);
+}
