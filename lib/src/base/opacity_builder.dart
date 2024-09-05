12da5fc3c68e2e2ec702a808576f3c0f4148907f
Barry <87644022+fastcode555@users.noreply.github.com>
2024-09-05 09:36:33 +0700

    refactor: let the flutter tailwind support opacity feature

--- lib/src/base/opacity_builder.dart
+++ lib/src/base/opacity_builder.dart
@@ -12,41 +12,41 @@ extension ColorOptionalOpacityExt on Color? {
 }
 
 extension ColorOpacityExt<T extends Color> on T {
-  T get opacity10 => this..withOpacity(0.1);
+  Color get opacity10 => this.withOpacity(0.1);
 
-  T get opacity15 => this..withOpacity(0.15);
+  Color get opacity15 => this.withOpacity(0.15);
 
-  T get opacity20 => this..withOpacity(0.2);
+  Color get opacity20 => this.withOpacity(0.2);
 
-  T get opacity25 => this..withOpacity(0.25);
+  Color get opacity25 => this.withOpacity(0.25);
 
-  T get opacity30 => this..withOpacity(0.3);
+  Color get opacity30 => this.withOpacity(0.3);
 
-  T get opacity35 => this..withOpacity(0.35);
+  Color get opacity35 => this.withOpacity(0.35);
 
-  T get opacity40 => this..withOpacity(0.4);
+  Color get opacity40 => this.withOpacity(0.4);
 
-  T get opacity45 => this..withOpacity(0.45);
+  Color get opacity45 => this.withOpacity(0.45);
 
-  T get opacity50 => this..withOpacity(0.5);
+  Color get opacity50 => this.withOpacity(0.5);
 
-  T get opacity55 => this..withOpacity(0.55);
+  Color get opacity55 => this.withOpacity(0.55);
 
-  T get opacity60 => this..withOpacity(0.6);
+  Color get opacity60 => this.withOpacity(0.6);
 
-  T get opacity65 => this..withOpacity(0.65);
+  Color get opacity65 => this.withOpacity(0.65);
 
-  T get opacity70 => this..withOpacity(0.7);
+  Color get opacity70 => this.withOpacity(0.7);
 
-  T get opacity75 => this..withOpacity(0.75);
+  Color get opacity75 => this.withOpacity(0.75);
 
-  T get opacity80 => this..withOpacity(0.8);
+  Color get opacity80 => this.withOpacity(0.8);
 
-  T get opacity85 => this..withOpacity(0.85);
+  Color get opacity85 => this.withOpacity(0.85);
 
-  T get opacity90 => this..withOpacity(0.9);
+  Color get opacity90 => this.withOpacity(0.9);
 
-  T get opacity95 => this..withOpacity(0.95);
+  Color get opacity95 => this.withOpacity(0.95);
 }
 
 mixin OpacityBuilder {
